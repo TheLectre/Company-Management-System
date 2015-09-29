871ca96c48769b9e0c48801b730b8bd92c17f96f
TheLectre <lectredev@gmail.com>
2015-09-29 10:36:06 +0200

    Update TeamViewFunctionality.java
    regular stuff

--- src/gui/employee_scene/functionalities/TeamViewFunctionality.java
+++ src/gui/employee_scene/functionalities/TeamViewFunctionality.java
@@ -1,3 +1,73 @@
 public class TeamViewFunctionality extends BasicFunctionality {
+    //================================================================================
+    // Fields
+    //================================================================================
+    private VBox mainPane;
+    private TableView<EmployeeDisplay> teamTable;
+    private Label noTeamLabel;
+    
+    //================================================================================
+    // Constructors
+    //================================================================================
+    public TeamViewFunctionality(EmployeeSceenRoot root, ResourceManager rManager) {
+      super(root, rManager);
+    }
+    
+    //================================================================================
+    // Methods
+    //================================================================================
+    @Override
+    public String name() {
+      return "Team View";
+    }
+    
+    @Override
+    public Pane loadContent() {
+      mainPane = new VBox();
+      
+      mainPane.setAlignment(Pos.CENTER);
+      
+      if(root.getEmployee().getTeam() != null) {
+      teamTable = createTeamTable();
+      mainPane.getChildren().add(teamTable);
+      }
+      
+      else {
+        noTeamLabel = createNoTeamLabel();
+        mainPane.getChildren.add(noTeamLabel);
+      }
+      
+    }
+    
+    public Label createNoTeamLabel() {
+      Label label = new Label("You do not belong to any team.");
+      
+      return label;
+    }
+    
+    public TableView createTeamTable() {
+      TableView tableView<EmployeeDisplay> = new TableView<>();
+      
+      ArrayList teamMembers = rManager.getEmployeesByTeamID(/*teamID*/ rManager.getEmployee().getTeam().getID());
 
+    
+
+      return tableView();
+    }
+    //================================================================================
+    // Accessors
+    //================================================================================
+    
+    //================================================================================
+    // Inner class
+    //================================================================================
+    private static class EmployeeDisplay {
+      private final int ID;
+      private final SimpleStringProperty firstName;
+      private final SimpleStringProperty lastName;
+      private final SimpleStringProperty technologyName;
+      private final SimpleStringProperty position;
+      
+    }
+  
 }
