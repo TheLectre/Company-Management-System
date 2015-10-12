73fa466d48eee21c692af69506f55e3d3144a965
TheLectre <lectredev@gmail.com>
2015-10-12 14:04:43 +0200

    regular

--- src/gui/employee_scene/functionalities/TechnologiesFunctioanlity.java
+++ src/gui/employee_scene/functionalities/TechnologiesFunctioanlity.java
@@ -39,7 +39,7 @@ public class TechnologiesFunctioanlity extends BaseFunctionality {
     public Pane loadContent() {
         mainPane = createMainPane();
         
-        technologyTable = createTechnologyTablePane();
+        technologyTable = createTechnologyTable();
         addTechnologyPane = createAddTechnologyPane();
         removeTechnologyPane = createRemoveTechnologyPane();
         
@@ -49,10 +49,49 @@ public class TechnologiesFunctioanlity extends BaseFunctionality {
         return mainPane;
     }
     
-    private TableView createTechnologyTablePane() {
-        TableView pane = new TableView();
+    private TableView createTechnologyTable() {
+        TableView<TechnologyDisplay> table = new TableView<>();
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         
-        return pane;
+        TableColumn<TechnologyDisplay, String> nameColumn = new TableColumn<>("Name");
+        table.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        TableColumn<TechnologyDisplay, Integer> numberOfTeamsColumn = new TableColumn<>("numberOfTeams");
+        table.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        TableColumn<TechnologyDisplay, Integer> numberOfEmployeesColumn = new TableColumn<>("numberOfEmployees");
+        table.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        tableView.getColumns().addAll(nameColumn, numberOfTeamsColumn, numberOfEmployeesColumn);
+        
+        return table;
+    }
+    
+    private void refreshTechnologyTable() {
+        ObservableList<TechnologyDisplay> data = FXCollections.observableArrayList();
+        
+
+        
+        
+        
+        for(Technology p : rManager.getAllTechnologies()) {
+            
+            int numberOfTeams = 0;
+            for(Team k : rManager.getAllTeams()) {
+             if(k.getTechnology().equals(p)) {
+                 numberOfTeams++;
+                }
+            }
+            
+            int numberOfEmployees = 0;
+            for(Employee k : rManager.getAllEmployees()) {
+             if(k.getTechnology().equals(p) && k.isAccepted) {
+                 numberOfEmployees++;
+                }
+            }
+            
+            data.add(new TechnologyDisplay(p.getName(), numberOfTeams, numberOfEmployees));
+        }
     }
     
     private VBox createAddTechnologyPane() {
@@ -72,6 +111,31 @@ public class TechnologiesFunctioanlity extends BaseFunctionality {
         
         return pane;
     }
+    //================================================================================
+    // Inner class
+    //================================================================================
     
-    
+    public class TechnologyDisplay {
+        private final SimpleStringProperty name;
+        private final int numberOfTeams;
+        private final int numberOfEmployees;
+        
+        public TechnologyDisplay(String name, int numberOfTeams, int numberOfEmployees) {
+            this.name = new SimpleStringProperty(name);
+            this.numberOfTeams = numberOfTeams;
+            this.numberOfEmployees = numberOfEmployees;
+        }
+        
+        public String getName() {
+            return name.get();
+        }
+        
+        public int getNumberOfTeams() {
+            return numberOfTeams;
+        }
+        
+        public int getNumberOfEmployees() {
+            return numberOfEmployees;
+        }
+    }
 }
